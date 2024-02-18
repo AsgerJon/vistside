059e74a8634890aaa3270ec791838a485f3bf920
Asger Jon Vistisen <asgerjon2@gmail.com>
2024-02-18 13:07:26 +0100

    Changes to pycharm .idea and main.py
    No changes made to the actual code, only to the testing script and to the pycharm settings

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 """Main Tester Script"""
+#  MIT Licence
+#  Copyright (c) 2024 Asger Jon Vistisen
+from __future__ import annotations
+import os
+import sys
+
+from icecream import ic
+
+ic.configureOutput(includeContext=True)
+
+
+def tester00() -> None:
+  """Hello world"""
+  items = [os, sys, 'hello world']
+  for item in items:
+    ic(item)
+
+
+if __name__ == '__main__':
+  tester00()
